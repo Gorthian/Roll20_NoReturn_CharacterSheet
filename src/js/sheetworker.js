cdaca27002b5954d3c90506b7298b4780e657dd3
Gorthian <74892068+Gorthian@users.noreply.github.com>
2022-10-27 18:31:47 +0200

    Umstellung auf PUG
    Erste Versuche den Bogen auf PUG umzustellen

---
+++ src/js/sheetworker.js
@@ -0,0 +1,10 @@
+/* TAB MENU */
+const buttonlist = ["page-one","page-two"];
+buttonlist.forEach(button => {
+    on(`clicked:${button}`, function() {
+        console.log(button);
+        setAttrs({
+            sheetTab: button
+        });
+    });
+});
