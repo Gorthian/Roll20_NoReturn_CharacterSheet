b0c9263bf704aee822845b1743350515c1e0c6d7
Gorthian <74892068+Gorthian@users.noreply.github.com>
2023-01-15 18:20:45 +0100

    Fertigkeitsproben
    Rolltemplate und Buttons vorbereitet.
    Noch keine Funktion
    
    #4

--- src/js/sheetworker.js
+++ src/js/sheetworker.js
@@ -1,10 +1,80 @@
 /* TAB MENU */
-const buttonlist = ["page-one","page-two"]; //Bei Änderungen auch doe globale Variable in noreturn.pug anpassen
+const buttonlist = ["page-one","page-two"]; //Bei Änderungen auch die globale Variable in noreturn.pug anpassen
 buttonlist.forEach(button => {
     on(`clicked:${button}`, function() {
         console.log(button);
         setAttrs({
             sheetTab: button
         });
+    });
+});
+
+/* Rolltemplates */
+const skilllist = [
+    ['akrobatik','beweglichkeit'],
+    ['astronomie','wissen'],
+    ['akrobatik','beweglichkeit'],
+    ['ausdauer','konstitution'],
+    ['ausweichen','beweglichkeit'],
+    ['basteln','handwerk'],
+    ['blocken','beweglichkeit'],
+    ['bodenfahrzeuge','geschick'],
+    ['chemie','wissen'],
+    ['computer','wissen'],
+    ['dressieren','charisma'],
+    ['einschuechtern','charisma'],
+    ['einschuechtern','staerke'],
+    ['elektronik','handwerk'],
+    ['feuerwaffenbau','handwerk'],
+    ['fluggeraete','geschick'],
+    ['gassenwissen','wissen'],
+    ['geschuetze','geschick'],
+    ['grossraumschiff','wissen'],
+    ['heimlichkeit','wahrnehmung'],
+    ['inspiration','charisma'],
+    ['klettern','beweglichkeit'],
+    ['kunst','handwerk'],
+    ['mechanik','handwerk'],
+    ['medizin','wissen'],
+    ['menschenkenntnis','charisma'],
+    ['nachforschen','wahrnehmung'],
+    ['nahkampf','beweglichkeit'],
+    ['naturkunde','wissen'],
+    ['orientierung','wahrnehmung'],
+    ['projektilwaffen','geschick'],
+    ['raumjaeger','geschick'],
+    ['raumschiff','geschick'],
+    ['reaktion','wahrnehmung'],
+    ['reiten','beweglichkeit'],
+    ['rennen','beweglichkeit'],
+    ['robotik','handwerk'],
+    ['ruestungsbau','handwerk'],
+    ['sagen-mythen','wissen'],
+    ['saufen','konstitution'],
+    ['schaetzen','wahrnehmung'],
+    ['schauspiel','charisma'],
+    ['schiessen','geschick'],
+    ['schwere-waffen','geschick'],
+    ['schwimmen','beweglichkeit'],
+    ['sicherheitstechnik','handwerk'],
+    ['spezies','wissen'],
+    ['springen','beweglichkeit'],
+    ['spruehwaffen','geschick'],
+    ['stemmen','staerke'],
+    ['taschendiebstahl','geschick'],
+    ['ueberreden','charisma'],
+    ['verhandeln','charisma'],
+    ['waffenbau','handwerk'],
+    ['waffenkampf','beweglichkeit'],
+    ['wasserfahrzeuge','geschick'],
+    ['willenskraft','wissen'],
+    ['wurfwaffen','geschick'],
+    ['zero-g','beweglichkeit'],
+];
+
+skilllist.forEach(skills => {
+    let skill = skills[0];
+    on(`clicked:offen-${skill}`, function() {
+        console.log("Roll "+skill);
     });
 });
